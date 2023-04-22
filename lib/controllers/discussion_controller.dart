a7e13c20c71f16f2cd36ffea5897d62a254c0dd7
Aya Ahmed <79098945+AyaFayed@users.noreply.github.com>
2023-04-23 00:13:16 +0200

    Update discussion_controller.dart

--- lib/controllers/discussion_controller.dart
+++ lib/controllers/discussion_controller.dart
@@ -1,1 +1,105 @@
-class DiscussionController {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:guc_scheduling_app/controllers/user_controller.dart';
+import 'package:guc_scheduling_app/database/database.dart';
+import 'package:guc_scheduling_app/models/course/course_model.dart';
+import 'package:guc_scheduling_app/models/discussion/post_model.dart';
+import 'package:guc_scheduling_app/shared/constants.dart';
+
+class DiscussionController {
+  final UserController _user = UserController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future createPost(String content, String? file, String courseId) async {
+    final docPost = Database.posts.doc();
+
+    final post = Post(
+        id: docPost.id,
+        content: content,
+        file: file,
+        author: _auth.currentUser?.uid ?? '',
+        replies: [],
+        createdAt: DateTime.now());
+
+    final json = post.toJson();
+
+    await docPost.set(json);
+
+    await addPostToCourse(docPost.id, courseId);
+
+    return null;
+  }
+
+  Future addPostToCourse(String postId, String courseId) async {
+    Course? course = await Database.getCourse(courseId);
+    if (course != null) {
+      List<String> posts = course.posts;
+      posts.add(postId);
+      await Database.courses.doc(courseId).update({'posts': posts});
+    }
+  }
+
+  Future addReplyToPost(String content, String postId) async {
+    Reply reply = Reply(
+        content: content,
+        author: _auth.currentUser?.uid ?? '',
+        createdAt: DateTime.now());
+
+    Post? post = await Database.getPost(postId);
+    if (post != null) {
+      List<Reply> replies = post.replies;
+      replies.add(reply);
+      await Database.posts.doc(postId).update({'replies': replies});
+    }
+  }
+
+  Future deletePost(String postId, String courseId) async {
+    Post? post = await Database.getPost(postId);
+    Course? course = await Database.getCourse(courseId);
+
+    if (post != null && course != null) {
+      if (post.author != _auth.currentUser?.uid) {
+        UserType userType = await _user.getCurrentUserType();
+        if (userType == UserType.student) return;
+      }
+
+      List<String> posts = course.posts;
+
+      posts.remove(postId);
+
+      await Database.courses.doc(courseId).update({'posts': posts});
+
+      await Database.posts.doc(postId).delete();
+    }
+  }
+
+  Future deleteReply(String postId, Reply reply) async {
+    Post? post = await Database.getPost(postId);
+
+    if (post != null) {
+      if (reply.author != _auth.currentUser?.uid) {
+        UserType userType = await _user.getCurrentUserType();
+        if (userType == UserType.student) return;
+      }
+
+      List<Reply> replies = post.replies;
+
+      replies.removeWhere((r) =>
+          r.author == reply.author &&
+          r.createdAt == reply.createdAt &&
+          r.content == reply.content);
+
+      await Database.posts.doc(postId).update({'replies': replies});
+    }
+  }
+
+  Future<List<Post>> getCoursePosts(String courseId) async {
+    Course? course = await Database.getCourse(courseId);
+
+    if (course != null) {
+      List<Post> posts = await Database.getPostListFromIds(course.posts);
+      return posts;
+    }
+
+    return [];
+  }
+}
